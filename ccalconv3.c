2a7953711665a1ab8deba6004d2b6358bdcc7420
Makoto Tanabe <xekid78@outlook.com>
2018-03-23 11:30:26 +0900

    Add files via upload

---
+++ ccalconv3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    srand((unsigned int)time(NULL));
+    int seireki = rand() % 63 + 1926;
+    printf("西暦%d年は、", seireki);
+
+    // 昭和年を計算
+    int showa = seireki - 1925;
+    printf("昭和%d年です\n", showa);
+}
